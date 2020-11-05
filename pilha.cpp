ec2a52d2e427e631ee81dc5aa08989115f76997f
DHLS <55817197+DougHigashi@users.noreply.github.com>
2020-11-05 14:35:41 -0300

    Organizado os prints no main

--- pilha.cpp
+++ pilha.cpp
@@ -8,7 +8,7 @@ void exibirPilha(struct pilha *pPilha);
 
 struct pilha{
 	int topo;
-	int nums[5];
+	int nums[];
 };
 
 struct pilha minhaPilha;
@@ -21,9 +21,11 @@ main(){
 	empilha(pp, 2);
 	empilha(pp, 10);//10 é o topo da pilha
 	exibirPilha(pp);
+	printf("\n");
 	
 	desempilha(pp);//retiro o 10 do topo. Agora o 2 e o topo
 	exibirPilha(pp);
+	printf("\n");
 	
 	empilha(pp, 99);//coloco 99 no topo da pilha
 	exibirPilha(pp);
@@ -41,7 +43,7 @@ void empilha(struct pilha *pPilha, int num){
 void desempilha(struct pilha *pPilha){
 	int aux = pPilha->nums[pPilha->topo];
 	pPilha->topo--;
-	printf("\nNumero %d desempilhado\n", aux);
+	printf("Numero %d desempilhado\n", aux);
 }
 
 void exibirPilha(struct pilha *pPilha){
